1157ba5c0d33ca0622f75ea492a58b6d0127d067
Brendan Kolisnik <bkolisnik@gmail.com>
2016-12-27 00:58:09 -0500

    Beginning game logic for piece movement and removal

--- src/chess/ChessBoard.java
+++ src/chess/ChessBoard.java
@@ -1,15 +1,19 @@
 package chess;
 
+import java.util.LinkedList;
+
+
 public class ChessBoard {
 	
 	Box[][] m_boxes;
-	Boolean m_rowEven;
+	private Boolean m_rowEven;
+	private LinkedList<Piece> m_removedPieces = new LinkedList<Piece>();
 	
 	//0 for black, 1 for white
 	
 	public ChessBoard(){
 		
-		initializeBoard();
+		initializeChessBoard();
 		
 	}
 	
@@ -18,11 +22,7 @@ public class ChessBoard {
 		//initializePieces();
 	}
 	
-	private void initializePieces(){
-		
-		
-		
-	}
+	
 	
 	private void initializeBoard(){
 		
@@ -94,5 +94,57 @@ public class ChessBoard {
 			
 		}
 	}
+	
+	
+	private void removePiece(int row, int column){
+		
+		m_removedPieces.add(m_boxes[row][column].m_Piece);
+		m_boxes[row][column].m_Piece = null;
+		
+	}
+	
+	private boolean checkBoxForPiece(int row, int column){
+		if(m_boxes[row][column].m_Piece!= null){
+		
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
+	
+	private boolean canMoveToPosition(String PieceColour, int startRow,int startColumn,int destinationRow,int destinationColumn){
+		
+		//if tile not occupied by own piece and piece can move like that
+		
+		if(m_boxes[destinationRow][destinationColumn].m_Piece.m_colour == PieceColour){
+			return false;
+		}
+		else{
+			
+			return true;
+		}
+		
+		
+		//Implement pathing system for piece
+		
+		
+	}
+	
+	private void movePiece(int startRow,int startColumn,int destinationRow,int destinationColumn){
+		if(checkBoxForPiece(destinationRow,destinationColumn)){
+			removePiece(destinationRow,destinationColumn);
+			
+		}
+		
+		m_boxes[destinationRow][destinationColumn].m_Piece = m_boxes[startRow][startColumn].m_Piece;
+		m_boxes[startRow][startColumn].m_Piece = null;
+	}
+	
+	
+	
+	
+	
 	
 }
